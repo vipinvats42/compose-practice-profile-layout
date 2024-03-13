e5de7fa07772949dab590aca111b67bfc331c14a
Vipin <vignesh.raju@daimler.com>
2024-03-13 23:08:14 +0530

    create theme

--- app/src/main/java/com/test/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/test/profilecardlayout/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,18 +23,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ContentAlpha
+import com.test.profilecardlayout.ui.theme.MyTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            MyTheme {
+                MainScreen()
+            }
         }
     }
 }
@@ -50,7 +56,19 @@ fun MainScreen() {
 
 @Composable
 fun ProfileContent() {
-    Text(text = "Vipin tyagi1")
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(text = "Vipin tyagi", style = MaterialTheme.typography.bodyLarge)
+        Text(
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.alpha(ContentAlpha.disabled),
+            text = "Active Now"
+        )
+    }
+
 }
 
 @Composable
@@ -76,13 +94,17 @@ fun ProfilePicture() {
 @Composable
 fun ProfileCard() {
     Card(
-        modifier = Modifier.fillMaxSize()
-            .padding(16.dp).wrapContentHeight(align = Alignment.Top),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .wrapContentHeight(align = Alignment.Top),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        Row(modifier = Modifier.fillMaxWidth(),
+        Row(
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start) {
+            horizontalArrangement = Arrangement.Start
+        ) {
             ProfilePicture()
             ProfileContent()
         }
@@ -92,5 +114,8 @@ fun ProfileCard() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MainScreen()
+    MyTheme {
+        MainScreen()
+    }
+
 }
